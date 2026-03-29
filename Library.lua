be80417235e99fe267c4042781cd631d40a3dcad
AlwaysLose - Developer <garild.zig.1@gmail.com>
2026-03-29 16:56:50 +0300

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -3155,8 +3155,8 @@ function Library:CreateWindow(...)
 
     local TabArea = Library:Create('Frame', {
         BackgroundTransparency = 1;
-        Position = UDim2.new(0, 10, 0, 21);
-        Size = UDim2.new(1, -20, 0, 21);
+        Position = UDim2.new(0, 8, 0, 8);
+        Size = UDim2.new(1, -16, 0, 21);
         ZIndex = 1;
         Parent = MainSectionInner;
     });
